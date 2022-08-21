19b6414418588d19f8216c89cb2f8b37047b653f
Jeremy <jeremywib7@gmail,com>
2022-08-21 23:49:43 +0700

    sum test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,8 +6,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
+	Calculator underTest = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void itShouldAddNumbers() {
+		// given
+		int numberOne = 20;
+		int numberTwo = 30;
+
+		//when
+
+	}
+
+	class Calculator {
+		int add(int a, int b) {
+			return a + b;
+		}
 	}
 
 }
