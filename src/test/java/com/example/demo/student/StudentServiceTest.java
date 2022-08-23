cead43df3fdf1999d4e3f886be067c510fae906a
Jeremy <jeremywib7@gmail,com>
2022-08-24 00:01:45 +0700

    test get all student

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,29 +1,43 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
 @DataJpaTest
 class StudentServiceTest {
 
     @Mock
     private StudentRepository studentRepository;
+    private AutoCloseable autoCloseable;
     private StudentService underTest;
 
     @BeforeEach
     void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
         underTest = new StudentService(studentRepository);
     }
 
-    @Test
-    void getAllStudents() {
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
 
+    @Test
+    void canGetAllStudents() {
+        // when
+        underTest.getAllStudents();
+
+        // then
+        verify(studentRepository).findAll();
     }
 
     @Test
